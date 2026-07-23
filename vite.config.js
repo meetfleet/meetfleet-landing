2a9a9e55816ec7c18240ca3030063c63d389d9c9
meetfleet <bacchuuus@gmail.com>
2026-07-23 21:22:11 +0100

    feat: configure HTTPS SSL dev server support

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+
+const keyPath = '/Users/mac/Downloads/private_50ec3d72-27b2-450a-82d2-b86fe1fd4df2(1).key'
+const certPath = '/Users/mac/Downloads/ssl/meetfleet_org.crt'
+const caPath = '/Users/mac/Downloads/ssl/meetfleet_org.ca-bundle'
+
+const hasCert = fs.existsSync(keyPath) && fs.existsSync(certPath)
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: hasCert ? {
+      key: fs.readFileSync(keyPath),
+      cert: fs.readFileSync(certPath),
+      ca: fs.existsSync(caPath) ? fs.readFileSync(caPath) : undefined,
+    } : false,
+  },
 })
